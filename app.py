5ea12ac382874a3d3e17e474fd7b717d155d6796
Guilherme Azambuja <guilhermevazambuja@gmail.com>
2023-06-03 00:10:20 -0300

    profile info now saved in cookies

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from csv import reader, writer
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, Response, redirect
 
 app = Flask(__name__)
 
@@ -12,7 +12,51 @@ CSV_FILENAME = "votes.csv"
 
 @app.route("/")
 def main() -> str:
-    return render_template("index.html")
+    username = request.cookies.get("username")
+    if username is None:
+        return render_template("index.html")
+    else:
+        userpic = request.cookies.get("userpic")
+        if userpic == "monster1":
+            userpic_path = "../static/images/monster1.png"
+        elif userpic == "monster2":
+            userpic_path = "../static/images/monster2.png"
+        else:
+            userpic_path = "../static/images/monster3.png"
+        userclrm = request.cookies.get("userclrm")
+        return render_template("voting.html", username=username, userpic_path=userpic_path, userclrm=userclrm)
+
+
+@app.route("/login", methods=['POST'])
+def save_login() -> Response:
+    name = request.form['name']
+    password = request.form['password']
+    response = make_response(render_template("user_cfg.html", username=name))
+    response.set_cookie("username", name)
+    response.set_cookie("userpswd", password)
+    return response
+
+
+@app.route("/logout")
+def delete_cookie() -> Response:
+    response = make_response(redirect("/"))
+    response.set_cookie("username", "", expires=0)
+    response.set_cookie("userpswd", "", expires=0)
+    response.set_cookie("userclrm", "", expires=0)
+    response.set_cookie("userpic", "", expires=0)
+    return response
+
+
+@app.route("/save_user_cfg", methods=['POST'])
+def vote_page() -> Response:
+    username = request.form['name']
+    userpic = request.form['profile_pic']
+    userclrm = request.form['color_mode']
+    response = make_response(redirect("/"))
+    response.set_cookie("username", username)
+    response.set_cookie("userpic", userpic)
+    response.set_cookie("userclrm", userclrm)
+    return response
 
 
 @app.route("/vote", methods=['POST'])
@@ -29,7 +73,7 @@ def vote() -> str:
     option = int(request.form['option'][-1])
     data[option][1] += 1
 
-    with open('votes.csv', 'w', newline='') as csvfile:
+    with open(CSV_FILENAME, 'w', newline='') as csvfile:
         csv_writer = writer(csvfile)
         csv_writer.writerows(data)
 
